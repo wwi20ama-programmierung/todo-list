3d1e63187c2773e54cf592e5ba86b9dff6ca9a6f
repl.it user <replituser@example.com>
2021-02-27 14:21:03 +0000

    Basis-Implementierung der Listen-Klasse.

--- src/de/dhbw/Liste.java
+++ src/de/dhbw/Liste.java
@@ -1,4 +1,24 @@
 package de.dhbw;
 
+import java.util.ArrayList;
+
 public class Liste {
+  private String name;
+  private Benutzer owner;
+  private ArrayList<Aufgabe> tasks;
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public ArrayList<Aufgabe> getTasks()
+  {
+    return tasks;
+  }
+
+  public void addAufgabe(Aufgabe aufgabe)
+  {
+    tasks.add(aufgabe);
+  }
 }
