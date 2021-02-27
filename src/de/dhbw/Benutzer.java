b50d8b5ea623d956dc906d3f0f107c225ecb49dc
repl.it user <replituser@example.com>
2021-02-27 14:18:08 +0000

    Basisimplementierung der Benutzer-Klasse

--- src/de/dhbw/Benutzer.java
+++ src/de/dhbw/Benutzer.java
@@ -1,4 +1,18 @@
 package de.dhbw;
 
+import java.util.ArrayList;
+
 public class Benutzer {
+  private String name;
+  private ArrayList<Liste> listen;
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void setList(Liste liste)
+  {
+    listen.add(liste);
+  }
 }
