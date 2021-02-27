13e30331ad93894dbe22e8a0b6d6d78dacf86d37
repl.it user <replituser@example.com>
2021-02-27 14:29:59 +0000

    Basis-Implementierung der Aufgaben-Klasse hinzugefügt.

--- src/de/dhbw/Aufgabe.java
+++ src/de/dhbw/Aufgabe.java
@@ -1,4 +1,26 @@
 package de.dhbw;
 
 public class Aufgabe {
+  private String description;
+  private Datum todoUntil;
+
+  public String getDescription()
+  {
+    return description;
+  }
+
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  public Datum getTodoUntil()
+  {
+    return todoUntil;
+  }
+
+  public void setTodoUntil(Datum datum)
+  {
+    todoUntil = datum;
+  }
 }
