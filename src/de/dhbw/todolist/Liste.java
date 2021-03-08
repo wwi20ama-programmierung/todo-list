cdfec5987d8d61c0bbd395f86de8abfb9d46ed4c
laundy <developing@laundy.de>
2021-03-08 09:39:16 +0100

    Rahmengerüst für lineare Suchmethoden

--- src/de/dhbw/todolist/Liste.java
+++ src/de/dhbw/todolist/Liste.java
@@ -3,42 +3,69 @@ package de.dhbw.todolist;
 import java.util.ArrayList;
 
 public class Liste {
-  private String name;
-  private ArrayList<Aufgabe> tasks;
+    private String name;
+    private ArrayList<Aufgabe> tasks;
 
-  public Liste()
-  {
-    tasks = new ArrayList<Aufgabe>();
-  }
-
-  public void setName(String name)
-  {
-    this.name = name;
-  }
-
-  public ArrayList<Aufgabe> getTasks()
-  {
-    return tasks;
-  }
-
-  public void addAufgabe(Aufgabe aufgabe)
-  {
-    tasks.add(aufgabe);
-  }
-
-  public void deleteAufgabe(Aufgabe aufgabe) {
-    tasks.remove(aufgabe);
-  }
-
-  public String toString()
-  {
-    String ergebnis = name + "\n";
-    for (Aufgabe task : tasks)
-    {
-      ergebnis += "  ";
-      ergebnis += task.toString();
-      ergebnis += "\n";
+    public Liste() {
+        tasks = new ArrayList<Aufgabe>();
     }
-    return ergebnis;
-  }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Aufgabe> getTasks() {
+        return tasks;
+    }
+
+    public void addAufgabe(Aufgabe aufgabe) {
+        tasks.add(aufgabe);
+    }
+
+    public void deleteAufgabe(Aufgabe aufgabe) {
+        tasks.remove(aufgabe);
+    }
+
+    /**
+     * Gibt ein Aufgabe-Objekt zurück, wenn der String "such" ein Teil der Aufgabenbeschreibung ist,
+     * ansonsten "null".
+     *
+     * @param such
+     * @return
+     */
+    public Aufgabe searchByDescription(String such) {
+        return null;
+    }
+
+    /**
+     * Gibt ein Aufgabe-Objekt zurück, wenn der Tag "tag" der Aufgabe zugeordnet ist,
+     * ansonsten "null".
+     *
+     * @param tag
+     * @return
+     */
+    public Aufgabe searchByTag(Tag tag) {
+        return null;
+    }
+
+    /**
+     * Gibt ein Aufgabe-Objekt zurück, wenn die Aufgabe die Priorität "priority" besitzt,
+     * ansonsten "null".
+     *
+     * @param priority
+     * @return
+     */
+    public Aufgabe searchByPriority(Priority priority) {
+        return null;
+    }
+
+    public String toString() {
+        String ergebnis = name + "\n";
+        for (Aufgabe task : tasks) {
+            ergebnis += "  ";
+            ergebnis += task.toString();
+            ergebnis += "\n";
+        }
+        return ergebnis;
+    }
 }
--- src/de/dhbw/todolist/TodoList.java
+++ src/de/dhbw/todolist/TodoList.java
@@ -7,54 +7,64 @@ public class TodoList {
 
     public static void main(String[] args) {
 
-      // Tags festlegen
-      Tag freizeit = new Tag("Freizeit", Color.GREEN);
-      Tag arbeit = new Tag("Arbeit");
-      Tag jahresziele = new Tag("Jahresziele", Color.RED);
+        // Tags festlegen
+        Tag freizeit = new Tag("Freizeit", Color.GREEN);
+        Tag arbeit = new Tag("Arbeit");
+        Tag jahresziele = new Tag("Jahresziele", Color.RED);
 
-      // Eine Todo-Liste erzeugen.
-      Liste l1 = new Liste();
+        // Eine Todo-Liste erzeugen.
+        Liste l1 = new Liste();
 
-      // Namen für die Liste vergeben.
-      l1.setName("Wichtige Aufgaben");
+        // Namen für die Liste vergeben.
+        l1.setName("Wichtige Aufgaben");
 
-      // Aufgaben hinzufügen.
-      Aufgabe a1 = new Aufgabe();
-      a1.setPriority(Priority.VERY_HIGH);
-      a1.setDescription("Im Lotto gewinnen");
-      a1.addTag(freizeit);
-      a1.addTag(jahresziele);
+        // Aufgaben hinzufügen.
+        Aufgabe a1 = new Aufgabe();
+        a1.setPriority(Priority.VERY_HIGH);
+        a1.setDescription("Im Lotto gewinnen");
+        a1.addTag(freizeit);
+        a1.addTag(jahresziele);
 
-      Aufgabe a2 = new Aufgabe();
-      a2.setPriority(Priority.LOW);
-      a2.setDescription("Dem Chef auf den Tisch ka...");
-      a2.addTag(arbeit);
+        Aufgabe a2 = new Aufgabe();
+        a2.setPriority(Priority.LOW);
+        a2.setDescription("Dem Chef auf den Tisch ka...");
+        a2.addTag(arbeit);
 
-      Aufgabe a3 = new Aufgabe();
-      a3.setPriority(Priority.VERY_HIGH);
-      a3.setDescription("Geld ausgeben");
-      a3.addTag(freizeit);
+        Aufgabe a3 = new Aufgabe();
+        a3.setPriority(Priority.VERY_HIGH);
+        a3.setDescription("Geld ausgeben");
+        a3.addTag(freizeit);
 
-      Aufgabe a4 = new Aufgabe();
-      a4.setPriority(Priority.VERY_LOW);
-      a4.setDescription("Job suchen");
-      a4.addTag(jahresziele);
-      
-      l1.addAufgabe(a1);
-      l1.addAufgabe(a2);
-      l1.addAufgabe(a3);
-      l1.addAufgabe(a4);
+        Aufgabe a4 = new Aufgabe();
+        a4.setPriority(Priority.VERY_LOW);
+        a4.setDescription("Job suchen");
+        a4.addTag(jahresziele);
 
-      System.out.println(l1);
+        l1.addAufgabe(a1);
+        l1.addAufgabe(a2);
+        l1.addAufgabe(a3);
+        l1.addAufgabe(a4);
 
-      l1.deleteAufgabe(a2);
+        System.out.println(l1);
 
-      // Erwartung: Aufgabe 2 nicht mehr angezeigt
-      System.out.println(l1);
+        l1.deleteAufgabe(a2);
 
-      a4.setDone();
+        // Erwartung: Aufgabe 2 nicht mehr angezeigt
+        System.out.println(l1);
 
-      // Erwartung: Aufgabe 4 ist erledigt
-      System.out.println(l1);
+        a4.setDone();
+
+        // Erwartung: Aufgabe 4 ist erledigt
+        System.out.println(l1);
+
+        // Suchmethoden:
+        l1.searchByDescription("Lotto"); // Erwartung: Referenz auf a1
+        l1.searchByDescription("Universität"); // Erwartung: null
+
+        l1.searchByTag(jahresziele); // Erwartung: Referenz auf a1
+        l1.searchByTag(new Tag("Jahresziele", Color.RED)); // Erwartung: Referenz auf a1
+
+        l1.searchByPriority(Priority.LOW); // Erwartung: Referenz auf a1
+        l1.searchByPriority(Priority.MEDIUM); // Erwartung: null
     }
 }
